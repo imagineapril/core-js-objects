e094b23d81420ee90708c86fb325f1ceba07898a
imagineapril <julia.timokhina604@gmail.com>
2025-03-04 22:15:02 +0300

    feat: add mergeObjects function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,14 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((acc, curent) => {
+    Object.entries(curent).forEach(([key, value]) => {
+      if (Object.keys(acc).includes(key)) acc[key] += value;
+      else acc[key] = value;
+    });
+    return acc;
+  }, {});
 }
 
 /**
